4c08fe7286531d38e45567930dfa007519dc48af
Salmansha08 <salmanwiharja8@gmail.com>
2025-08-02 14:58:29 +0700

    feat: implement Counter component with state management and reset functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,15 @@ import "@/public/styles/globals.css";
 import logo from "@/public/images/logo.svg";
 import reactLogo from "@/public/images/react.svg";
 import { Counter } from "./components/shared";
+import { useState } from "react";
 
 export function App() {
+  const [count, setCount] = useState(0);
+
+  const increment: () => void = () => setCount(count + 1);
+  const decrement: () => void = () => setCount(count - 1);
+  const reset: () => void = () => setCount(0);
+
   return (
     <div className="container mx-auto p-8 text-center relative z-10">
       <div className="flex justify-center items-center gap-8 mb-8">
@@ -20,7 +27,12 @@ export function App() {
         />
       </div>
 
-      <Counter />
+      <Counter
+        count={count}
+        onIncrement={increment}
+        onDecrement={decrement}
+        reset={reset}
+      />
     </div>
   );
 }
